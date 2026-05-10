e30c3b681ced5ab361e2fc7f55c08f909098ce63
naumsarti <naumsarti@gmail.com>
2026-05-09 21:36:54 -0300

    update estrutura

--- estrutura.py
+++ estrutura.py
@@ -9,3 +9,11 @@ class NodoFila:
         self.musica = musica
         self.proximo = None
 
+class Musica:
+    def __init__(self, id_musica, titulo, artista, genero, bpm):
+        self.id = id_musica
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
+        self.bpm = bpm
+
