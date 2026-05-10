518259d1146843c209a012b022b800cfd65ca3c4
naumsarti <naumsarti@gmail.com>
2026-05-10 17:30:55 -0300

    update estrutura

--- estrutura.py
+++ estrutura.py
@@ -22,3 +22,31 @@ class Biblioteca:
         self.head = None
         self._proximo_id = 1
         self.tamanho = 0
+
+    def existe(self, titulo, artista):
+        atual = self.head
+        while atual is not None:
+            m = atual.musica
+            if m.titulo.lower() == titulo.lower() and m.artista.lower() == artista.lower():
+                return True
+            atual = atual.proximo
+        return False
+
+    def inserir(self, titulo, artista, genero, bpm):
+        if self.existe(titulo, artista):
+            return None
+
+        nova_musica = Musica(self._proximo_id, titulo, artista, genero, bpm)
+        self._proximo_id += 1
+        novo_nodo = NodoLista(nova_musica)
+
+        if self.head is None:
+            self.head = novo_nodo
+        else:
+            atual = self.head
+            while atual.proximo is not None:
+                atual = atual.proximo
+            atual.proximo = novo_nodo
+            
+        self.tamanho += 1
+        return nova_musica
