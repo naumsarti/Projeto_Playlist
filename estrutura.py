b36545e0564cab503a2a30d51e751ecbab7c2e81
naumsarti <naumsarti@gmail.com>
2026-05-09 21:30:38 -0300

    Add files via upload

---
+++ estrutura.py
@@ -0,0 +1,11 @@
+class NodoLista:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
+
+
+class NodoFila:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
+
